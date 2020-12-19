bf24a6a9e5f8fbaba967ac80bbd3b60473026c90
Gabeta Soro <gabeta.soro@gmail.com>
2020-12-19 11:49:51 +0000

    test config

---
+++ src/GsmDetector.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Gabeta\GsmDetector;
+
+
+class GsmDetector
+{
+    /**
+     * @var array
+     */
+    private static $config;
+
+    public function __construct(array $config = null)
+    {
+    }
+
+    private static function setConfig(array $config)
+    {
+
+    }
+}
---
+++ tests/GsmDetectorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Gabeta\GsmDetector\Test;
+
+
+use PHPUnit\Framework\TestCase;
+
+class GsmDetectorTest extends TestCase
+{
+    public function test_default()
+    {
+        $this->assertTrue(true);
+    }
+}
